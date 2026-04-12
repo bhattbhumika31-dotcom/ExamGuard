f066c3f954aa1f7aa200a87eefec016df22c959c
Parth <bathamparth1@gmail.com>
2026-04-12 16:22:19 +0530

    added sql schema

---
+++ schema.sql
@@ -0,0 +1,22 @@
+create database examguard;
+use examguard;
+create table students(
+	ID integer primary key,
+    name varchar(20) not null,
+    class integer
+);
+
+create table teachers(
+	ID integer primary key,
+    name varchar(20) not null,
+    subject varchar(10),
+    phone integer
+);
+
+create table marks(
+	name varchar(20) not null,
+    ID integer primary key,
+    class integer,
+    marks float,
+    status boolean
+);
